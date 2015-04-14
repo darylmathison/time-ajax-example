493a185726a16360aea3cfa3752937a2de004f90
Daryl <daryl.mathison@gmail.com>
2015-04-13 20:58:42 -0500

    converted to Maven project

--- src/main/java/demo/ajax/TimeServlet.java
+++ src/main/java/demo/ajax/TimeServlet.java
@@ -1,14 +1,13 @@
 package demo.ajax;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * Servlet implementation class TimeServlet
